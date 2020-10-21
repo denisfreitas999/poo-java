8899cb0ee995da52e0cfe5ad0441c0da18583078
Denisson Freitas <denisson_sbuky@hotmail.com>
2020-10-21 16:00:33 -0300

    Inicio da utilização do Net Beans

---
+++ JAVA/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposprimitivos;
+
+/**
+ *
+ * @author denis
+ */
+public class TiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float nota = (float) 8.5; //pode ser escrito assim ou como se localiza abaixo
+        float nota2 = 8.5f;
+        System.out.print("A nota é: " + nota);
+    }
+    
+}
