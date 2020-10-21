cb5cee018780a0256d7865cc2af9ac38bbcc4fbd
Denisson Freitas <denisson_sbuky@hotmail.com>
2020-10-21 14:47:41 -0300

    Adicionado arquivo PrimeiroPrograma.java

--- JAVA/PrimeiroPrograma.java
+++ JAVA/PrimeiroPrograma.java
@@ -1,9 +1,12 @@
-package JAVA; //NOME DA PASTA
-public class PrimeiroPrograma{ // NOME DO ARQUIVO
+package JAVA; //NOME DA PASTA, pacote
+public class PrimeiroPrograma{ // NOME DO ARQUIVO, classe
     public static void main(String[] args) { // MÉTODO PRINCIPAL
         System.out.print("Olá, Mundo!\n"); // System.out.print(); Função utilizada para printar.
         System.out.println("Escreve e pula uma linha");
         System.out.print("Testando\n");
+
+        //System, primeira letra maiúscula = classe
+        //main, primeira letra minúscula = método
     }
     /* 
     Se a primeira letra for maiúscula é uma classe/interface : MinhaClasse , AlunosCursoEmVideo
