dcfcdab1935a6ccbb3e9deb41c7e56ceeb1bb980
Denisson Freitas <denisson_sbuky@hotmail.com>
2020-10-21 14:46:27 -0300

    JAVA para iniciantes adicionado

---
+++ JAVA/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package JAVA; //NOME DA PASTA
+public class PrimeiroPrograma{ // NOME DO ARQUIVO
+    public static void main(String[] args) { // MÉTODO PRINCIPAL
+        System.out.print("Olá, Mundo!\n"); // System.out.print(); Função utilizada para printar.
+        System.out.println("Escreve e pula uma linha");
+        System.out.print("Testando\n");
+    }
+    /* 
+    Se a primeira letra for maiúscula é uma classe/interface : MinhaClasse , AlunosCursoEmVideo
+
+    se a primeira letra for minúscula podemos ter:
+    meuAtributo : Atributo
+    minhaVariável : Variável
+    meuMétodo : Método
+    
+    */
+}
