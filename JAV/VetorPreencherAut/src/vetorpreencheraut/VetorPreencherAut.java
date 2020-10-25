f89beba5ca228ee7c04d51f9f54140ca06c5c5a6
Denisson Freitas <denisson_sbuky@hotmail.com>
2020-10-24 21:12:36 -0300

    Adicionado VetorPreencherAut program

---
+++ JAV/VetorPreencherAut/src/vetorpreencheraut/VetorPreencherAut.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vetorpreencheraut;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author denis
+ */
+public class VetorPreencherAut {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int v[] = new int[20];
+        Arrays.fill(v, 0);
+        
+        for(int vetor: v){
+            System.out.print(vetor + " ");
+        }
+    }
+    
+}
