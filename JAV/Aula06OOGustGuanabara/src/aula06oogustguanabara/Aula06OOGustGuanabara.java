aa8c3030e6f5cfbc196162ae2160646ffc7487f2
Denisson Freitas <denisson_sbuky@hotmail.com>
2020-11-30 19:48:43 -0300

    Aula 06 upada

---
+++ JAV/Aula06OOGustGuanabara/src/aula06oogustguanabara/Aula06OOGustGuanabara.java
@@ -0,0 +1,22 @@
+package aula06oogustguanabara;
+public class Aula06OOGustGuanabara {
+    
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
+        c.fecharMenu();
+        c.play();
+        c.menosVolume();
+        c.abrirMenu();
+        c.fecharMenu();
+        c.pause();
+        c.maisVolume();
+        c.maisVolume();
+        c.maisVolume();
+        c.maisVolume();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+    
+}
---
+++ JAV/Aula06OOGustGuanabara/src/aula06oogustguanabara/Controlador.java
@@ -0,0 +1,13 @@
+package aula06oogustguanabara;
+public interface Controlador {
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void  abrirMenu();
+    public abstract void fecharMenu();
+    public abstract void maisVolume();
+    public abstract void menosVolume();
+    public abstract void ligaMudo();
+    public abstract void delisgaMudo();
+    public abstract void play();
+    public abstract void pause();
+}
